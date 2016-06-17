975b29adabd135c034c26a10c13796946c79863e
ZachS456 <sanchezk765@gmail.com>
2016-06-17 17:14:00 -0500

    Mistake
    Mistake on last commit. Pushed wrong version of file

--- assignment-1/feed.php
+++ assignment-1/feed.php
@@ -48,9 +48,7 @@
 				echo "<hr style='border-color:gray' />";
 				$date = date('m/d/Y h:i A', strtotime($row[6]));
 				echo "<p>Likes: $row[5]</p> <p>$date</p>";
-				echo "<iframe>";
 				echo "<form action='likes.php' method='POST'> <input type='hidden' name='PID' value='$row[0]'> <input type='submit' value='Like'></form> <br />";
-				echo "</iframe>";
 				echo "<form method='POST' action='comment.php'>";
 				echo "<p><textarea rows='5' cols='40' name='comment' placeholder='What do you think?'></textarea></p>";
 				echo "<input type='hidden' name='UID' value='$UID'>";
